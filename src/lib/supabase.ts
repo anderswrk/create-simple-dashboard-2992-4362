58b70fbee38480e8baa912e7232c4f2bd51e784f
Kliv <noreply@kliv.dev>
2025-07-05 20:14:28 +0200

    Update Supabase key for enhanced security and access control

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zhmkzelpcmdoqpptnupx.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpobWt6ZWxwY21kb3FwcHRudXB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NDU3NjIsImV4cCI6MjA0OTUyMTc2Mn0.71cddbb852ea6ca5e0297aedab4cadc72ef511c70d82b6eff688abd1991726cb'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpobWt6ZWxwY21kb3FwcHRudXB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0MDA5NjIsImV4cCI6MjA1OTk3Njk2Mn0.qqJ2SwieZ1JdbiA9od1xsJwGvp0flOCCiIizNgeue3E'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
